16b07d6e4dd67d04485ff4dbccc7a4ffd71f112b
Milomir <milomir.dragovic@cubes.rs>
2018-06-08 14:31:11 +0200

    adds constructors for class Radio

--- src/media/Radio.java
+++ src/media/Radio.java
@@ -7,6 +7,23 @@ public class Radio {
     private int amFrequency;
     private char band;
 
+    public Radio() {
+        this.fmFrequency = 91.8;
+        this.amFrequency = 600;
+        this.band = 'F';
+    }
+
+    public Radio(double fmFrequency, int amFrequency, char band) {
+        this.fmFrequency = fmFrequency;
+        this.amFrequency = amFrequency;
+        this.band = band;
+    }
+
+    public Radio(double fmFrequency, int amFrequency) {
+        this.fmFrequency = fmFrequency;
+        this.amFrequency = amFrequency;
+    }
+
     public double getFmFrequency() {
         return fmFrequency;
     }
@@ -29,6 +46,12 @@ public class Radio {
 
     public void setBand(char band) {
         this.band = band;
+    }
+    
+    public void info() {
+        System.out.println("FM: " + getFmFrequency());
+        System.out.println("AM: " + getAmFrequency());
+        System.out.println("FM: " + getBand());
     }
     
     
